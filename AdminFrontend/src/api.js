3fde6ab60f0d7dbbd1f1acbeedab5e403f7efb6a
utkarsh955534 <utkarshmishra1012005@gmail.com>
2026-04-27 04:21:14 +0530

    Change API base URL to production server

--- AdminFrontend/src/api.js
+++ AdminFrontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://shopora-e-commerce-website.onrender.com",
 });
 
 // token automatically attach
